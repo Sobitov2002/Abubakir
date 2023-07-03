bc7b32d05c7435c00c28f80e8db6d86e7244dff4
AbubakirDeveloper <99266681+Sobitov2002@users.noreply.github.com>
2023-07-03 06:35:24 +0300

    Aa

--- script/app.js
+++ script/app.js
@@ -6,3 +6,30 @@ window.addEventListener("scroll", function(){
         header.classList.remove('sticky');
     }
 })
+
+
+// let navBurger = document.getElementById('nav_burger')
+// let navMenu = document.getElementsByClassName('.navigation_menu_items')
+// console.log(navMenu)
+
+// navBurger.addEventListener("click", function(){
+//     navMenu.classList.add('fa-xmark');
+//     console.log("salom");
+// });
+let navBurger = document.getElementById('nav_burger');
+let navMenu = document.getElementById('navigation_items')
+let burgerIcon = document.getElementById('burger_icon')
+let number = false;
+ function navburgertoggle(){
+   if (number == false){
+    burgerIcon.classList.add('fa-xmark')
+    navMenu.classList.add('burger_active')
+    number = true;
+   }
+   else
+   if(number == true){
+    navMenu.classList.remove('burger_active')
+    burgerIcon.classList.remove('fa-xmark');
+    number = false;
+   }
+ }
